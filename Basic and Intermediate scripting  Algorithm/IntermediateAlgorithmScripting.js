9cbbbab92ba81fba85d41dfb701aa3f1f432c546
niquepg <l.w.alexander@hotmail.com>
2017-01-06 00:47:59 -0500

    completed roman numeral coverter

--- Basic and Intermediate scripting  Algorithm/IntermediateAlgorithmScripting.js
+++ Basic and Intermediate scripting  Algorithm/IntermediateAlgorithmScripting.js
@@ -44,30 +44,42 @@ console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));//should return [4]*/
 /*Convert the given number into a roman numeral.*/
 
 function convertToRoman(num) {
-    var resultStr="", str=num.toString();
+    var resultAry=[], str=num.toString(),
+        over9thousand="", placeValue=0;;
+
     var helperDictionary={
-        1:"I",2:"II", 3:"III",4:"IV",5:"V",
-        6:"VI",7:"VII",8:"VIII",9:"IX",10:"X",
-        20:"XX",30:"XXX",40:"XL",60:"LX",70:"LXX",
-        80:"LXXX",90:"XC",100:"C",200:"CC",
-        300:"CCC",400:"CD",500:"D",600:"DC",
-        700:"DCC",800:"DCCC",900:"CM", 1000:"M"
+        "1":"I","2":"II", "3":"III","4":"IV","5":"V",
+        "6":"VI","7":"VII","8":"VIII","9":"IX","10":"X",
+        "20":"XX","30":"XXX","40":"XL","60":"LX","70":"LXX",
+        "80":"LXXX","90":"XC","100":"C","200":"CC",
+        "300":"CCC","400":"CD","500":"D","600":"DC",
+        "700":"DCC","800":"DCCC","900":"CM", "1000":"M"
     };
 
     var letterMaker = function(n,str){
         return new Array(n).join(str); 
     }
 
-    for(var i=0;i<str.length;i++){
-
+    if(str.length>=4){
+        //-3 because we want all number that are in the thousand place
+        over9thousand=str.slice(0, str.length-3);
+        console.log(over9thousand);
+        resultAry.push(letterMaker(parseInt(over9thousand)+1, 
+                helperDictionary["1000"]));
+        
+        str=str.slice(str.length-3); //update
     }
 
-
-    return zeroMaker(2);
+    placeValue=str.length;
+    for(var i=0;i<str.length;i++){
+        resultAry.push(helperDictionary[str[i]+letterMaker(placeValue,"0")]);
+        placeValue--;
+    }
+    return resultAry.join("");
 }
 
-console.log(convertToRoman(5));
-console.log(convertToRoman(45));
+//console.log(convertToRoman(9));
+console.log(convertToRoman(5145));
 //console.log(convertToRoman(501));
 //console.log(convertToRoman(1006));
 //console.log(convertToRoman(3999));
