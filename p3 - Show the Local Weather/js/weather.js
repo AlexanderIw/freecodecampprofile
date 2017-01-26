15ebe0592c9848ae2150331379fd77f464817665
niquepg <l.w.alexander@hotmail.com>
2017-01-26 18:34:47 -0500

    completed feature background image change depending on user local weather

--- p3 - Show the Local Weather/js/weather.js
+++ p3 - Show the Local Weather/js/weather.js
@@ -1,24 +1,26 @@
 var theWeather= function(){
     var xhr= new XMLHttpRequest();
-    var lat=0, lon=0, degree={};
+    var lat=0, lon=0,degree={};
     var weather  = document.getElementById("weather");
     var location = document.getElementById("location");
     var icon = document.getElementById("weather-icon");
     var description=document.getElementById("description");
     var mydegree= document.getElementById("degree");
-    var arrayColors=[{"thunder":"1a2844"}];
+    var myimage= document.getElementById("main-body");
+    var imageArray=["cloud","thunder", "rain", "snow","fog"];
+    //var arrayColors=[{"thunder":"1a2844"}]; coming soon 
 
-    var relationMapperOpenWeatherToIcons={
+    var openWeatherToIcons={
         "01d":"wi wi-day-sunny",
         "01n":"wi wi-night-clear",
-        "02d":"wi wi-day-cloudy",
-        "02n":"wi wi-night-alt-cloudy",
+        "02d":"wi wi-day-cloudy cloud",
+        "02n":"wi wi-night-alt-cloudy cloud",
         "03d":"wi wi-cloud",
         "03n":"wi wi-cloud",
-        "04d":"wi wi-cloudy",
-        "04n":"wi wi-cloudy",
-        "09d":"wi wi-day-showers",
-        "09n":"wi wi-night-alt-showers",
+        "04d":"wi wi-cloudy cloud",
+        "04n":"wi wi-cloudy cloud",
+        "09d":"wi wi-day-showers rain",
+        "09n":"wi wi-night-alt-showers rain",
         "10d":"wi wi-day-rain",
         "10n":"wi wi-night-alt-rain",
         "11d":"wi wi-day-thunderstorm",
@@ -47,11 +49,23 @@ var theWeather= function(){
                     location.innerHTML=weatherObj.name;
                     weather.innerHTML=degree.f +" &#8457;";
                     description.innerHTML=weatherObj.weather[0].description;
-                    icon.className=relationMapperOpenWeatherToIcons[weatherObj.weather[0].icon];
+                    icon.className=openWeatherToIcons[weatherObj.weather[0].icon];
+
+                    
+                    myimage.style.backgroundImage=(function(){
+                        for(var i=0; i<imageArray.length; i++){
+                            var re= new RegExp(imageArray[i],'g');
+                            if(openWeatherToIcons[weatherObj.weather[0].icon].match(re)!==null){
+                                return "url('/img/"+imageArray[i]+".jpg') ";
+                            }
+                        }
+                        return  "url('/img/default.jpg')";
+                        //function been tested.... it pickup all weather codes
+                    })();
+                    
 
                     document.getElementById("convert").addEventListener("click", function(){
-                        console.log(this.value);
-
+                        //console.log(this.value);
                         if(this.value==='c'){
                              weather.innerHTML=degree.c+ " &#8451";
                              this.value='f'
@@ -63,14 +77,9 @@ var theWeather= function(){
                         }
                     });
                 }
-            }
+            }//end xhr func
         });
     }
-
-    var mapper ={
-
-    };
-
 }
 
 theWeather();
